338d36a969cd4fd109a3fc4629586214d2d254bc
yasinl1 <yasinlester@gmail.com>
2023-12-08 17:05:21 +0000

    hello yasin

---
+++ test3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void hello() {
+  std::cout <<  "Hello Yasin enter your age";
+  int age;
+  std::cin >> age;
+  std:: cout << " yasin is " << age << " years old";
+    
+}
+
+  
+int main() {
+
+  hello();
+ 
+
+}
+
+
