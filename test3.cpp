4636388cc1d59fd778bb606c9e1660a8eb9d264c
yasinl1 <yasinlester@gmail.com>
2023-12-08 17:38:59 +0000

    c++

--- test3.cpp
+++ test3.cpp
@@ -8,11 +8,37 @@ void hello() {
     
 }
 
+int whatsMyAge() {
+
+  std::cout <<  "Hello Yasin enter your age";
+  int age;
+  std::cin >> age;
+
+  return age;
+}
+
+std::string whatsMyName() {
+
+  std::cout << "Now enter your name: \n";
+  std::string name;
+  std::cin >> name;
+
+  return name;
+}
+
+void outputDetails(int age, std::string name) {
+
+  std::cout << "Your name is " << name << ", and you're " << age << " years old!";
+
+}
   
 int main() {
 
-  hello();
- 
+  int ageReceived = whatsMyAge();
+  std::string nameReceived = whatsMyName();
+
+  outputDetails(ageReceived, nameReceived);
+  
 
 }
 
